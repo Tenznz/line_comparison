157cb8cec2f499f30fdfc7c6979a6033a8bc3954
tenznz <dhugkar@gmail.com>
2022-01-22 01:19:11 -0800

    Added:line comparison,exception

--- line_comparison.py
+++ line_comparison.py
@@ -1,14 +1,35 @@
+import logging
 import math
 
 
 def get_line_distance(x_axis1, x_axis2, y_axis1, y_axis2):
-    line = math.sqrt((x_axis2 - x_axis1) * (x_axis2 - x_axis1) + (y_axis2 - y_axis1) * (y_axis2 - y_axis1))
-    print("distance-between (", x_axis1, ",", y_axis1, "),", "(", x_axis2, ",", y_axis2, ") is ", line)
+    return math.sqrt((x_axis2 - x_axis1) * (x_axis2 - x_axis1) + (y_axis2 - y_axis1) * (y_axis2 - y_axis1))
+
+
+def user_input():
+    try:
+        x1 = int(input("Enter x1 value"))
+        x2 = int(input("Enter x2 value"))
+        y1 = int(input("Enter y1 value"))
+        y2 = int(input("Enter y2 value"))
+
+    except Exception:
+        logging.exception("input error")
+
+    return get_line_distance(x1, x2, y1, y2)
+
+
+def compare_line(line_length1, line_length2):
+    if line_length1 > line_length2:
+        return line_length1
+    else:
+        return line_length2
 
 
 if __name__ == "__main__":
-    x1 = int(input("Enter x1 value"))
-    x2 = int(input("Enter x2 value"))
-    y1 = int(input("Enter y1 value"))
-    y2 = int(input("Enter y2 value"))
-    get_line_distance(x1, x2, y1, y2)
+    print("Enter first line")
+    line_length1 = user_input()
+    print("Enter second line")
+    line_length2 = user_input()
+    print(line_length1, "compare", line_length2, "greater length is", compare_line(line_length1, line_length2))
+
